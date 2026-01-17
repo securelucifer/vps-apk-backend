9f61b19e28aac7dfea7b11b138206a2ab2e0f6d5
securelucifer <secure.lucifer.pandey@gmail.com>
2026-01-17 14:29:07 +0530

    xdd

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = (uri) => {
   mongoose
     .connect(uri, {
-      dbName: "AndroidDBDmart",
+      dbName: "apkDBDmart",
     })
     .then((c) => console.log(`DB Connected to ${c.connection.host}`))
     .catch((e) => console.log(e));
